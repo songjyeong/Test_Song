1c1efd2af4000df7b16149df023b0244def09624
SongJiYeong <kr5489@naver.com>
2022-05-27 09:42:00 +0900

    second commit

--- src/test_git/Test_Git_Java.java
+++ src/test_git/Test_Git_Java.java
@@ -3,5 +3,19 @@ package test_git;
 public class Test_Git_Java {
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
 	}
 }
